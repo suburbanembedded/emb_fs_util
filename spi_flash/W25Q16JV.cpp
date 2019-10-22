961a9b6f137b2c4b560bdd725aaa23d7be157350
Jacob Schloss <jacob@schloss.io>
2019-10-21 19:03:55 -0700

    initial release

--- spi_flash/W25Q16JV.cpp
+++ spi_flash/W25Q16JV.cpp
@@ -1,3 +1,9 @@
+/**
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2019 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
 #include "W25Q16JV.hpp"
 
 #include "common_util/Byte_util.hpp"
--- spiffs_flash/spiffs_int_w25q16jv.cpp
+++ spiffs_flash/spiffs_int_w25q16jv.cpp
@@ -1,1 +1,7 @@
+/**
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2019 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
 #include "spiffs_int_w25q16jv.hpp"
