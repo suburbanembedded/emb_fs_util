8d8fd62837cfddb734a687e128e310e64df7d62b
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2025-07-20 01:13:36 -0700

    fix c-isms

--- lfs_util/lfs_file_move.c
+++ lfs_util/lfs_file_move.c
@@ -1,13 +1,13 @@
 #include "lfs_file_move.h"
 
-lfs_mlist* get_prev_node(lfs_mlist* const head, lfs_mlist* const node)
+struct lfs_mlist* get_prev_node(struct lfs_mlist* const head, struct lfs_mlist* const node)
 {
 	if( ! head )
 	{
 		return NULL;
 	}
 
-	lfs_mlist* prev = head;
+	struct lfs_mlist* prev = head;
 
 	while(prev->next && (prev->next != node) )
 	{
@@ -20,7 +20,7 @@ lfs_mlist* get_prev_node(lfs_mlist* const head, lfs_mlist* const node)
 int lfs_file_movehandle(lfs_t* const fs, lfs_file_t* const old_file_t, lfs_file_t* const new_file_t)
 {
 	#ifdef LFS_THREADSAFE
-		int ret = fs->cfg.lock();
+		int ret = fs->cfg->lock(fs->cfg);
 		if(ret < 0)
 		{
 			return ret;
@@ -28,7 +28,7 @@ int lfs_file_movehandle(lfs_t* const fs, lfs_file_t* const old_file_t, lfs_file_
 	#endif
 
 	// find prev node
-	lfs_file_t* prev = get_prev_node(fs->mlist, old_file_t);
+	lfs_file_t* prev = (lfs_file_t*)get_prev_node(fs->mlist, (struct lfs_mlist*)old_file_t);
 	if(prev == NULL)
 	{
 		return LFS_ERR_INVAL;
@@ -37,10 +37,10 @@ int lfs_file_movehandle(lfs_t* const fs, lfs_file_t* const old_file_t, lfs_file_
 	// insert new node in place of old node, clear old node
 	*new_file_t = *old_file_t;
 	prev->next = new_file_t;
-	*old_file_t = { };
+	*old_file_t = (lfs_file_t) { };
 
 	#ifdef LFS_THREADSAFE
-		ret = fs->cfg.unlock();
+		ret = fs->cfg->unlock(fs->cfg);
 		if(ret < 0)
 		{
 			return ret;
