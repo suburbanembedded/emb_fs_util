0bcf240115cbe553231a8db8fd49565aa9264949
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2025-07-19 22:19:15 -0700

    add to build and header

--- lfs_util/lfs_file_move.h
+++ lfs_util/lfs_file_move.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "lfs.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int lfs_file_movehandle(lfs_t* const fs, lfs_file_t* const old_file_t, lfs_file_t* const new_file_t);
+
+#ifdef __cplusplus
+}
+#endif
